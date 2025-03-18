75720d5b28d061f0128054b230f732605cf97bcd
eliewebavenue <elie@webavenue.agency>
2025-03-18 10:37:21 +0100

    feat(router): Add Router class with core routing methods
    - Implement `add()` method to register routes.
    - Implement `match()` method to match requested routes with internal routes.
    - Implement `loadRoutes()` method to load predefined routes when the application starts.

---
+++ core/Dipatcher.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace App\Core;
+
+use App\Core\Router;
+
+class Dispatcher {
+
+}
---
+++ core/Router.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Core;
+
+class Router {
+
+    private $routes = [];
+
+    public static function add($method, $uri, $controller) {
+        self::$routes[] = [
+            'method' => strtoupper($method),
+            'uri' => $uri, 
+            'controller' => $controller
+        ] 
+    }
+
+    public static function match($requestUrl, $requestMethod){
+        foreach (self::$routes as $route) {
+            if($route['uri'] == $requestUrl && $route['method'] == strtoupper($requestMethod)){
+                return $route;
+            }
+        }
+    }
+
+    public static function loadRoutes() {
+        self::add('GET', '/', 'HomeController@index')
+    }
+
+
+
+
+
+}
