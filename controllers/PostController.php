38f8195f684fec0e3a1b1301e17da91dbce4e8b9
eliewebavenue <elie@webavenue.agency>
2025-03-19 16:37:47 +0100

    Add controller post and helper to upload file

---
+++ controllers/PostController.php
@@ -0,0 +1,33 @@
+<?php 
+namespace Controllers;
+
+use Core\Controller;
+use Core\Helper;
+use Models\PostModel;
+
+public function addPost(){
+    if(!isset($_SESSION['user'])){
+       Helper::redirect('/login');
+    }
+
+    if(isset($_POST['legende']) && isset($_FILES['image'])) {
+        
+        $user_id = $_SESSION['user']['id'];
+        $legende = $_POST['legende'];
+        $image = $_FILES['image'];
+
+        $upload = Helper::uploadFile($image, "post-img");
+        
+        if(is_array($upload)){
+            $this->render('home', $upload );
+        }else{
+            $postModel = new PostModel();
+            $res = $postModel->addPost($imageName, $legende, $user_id);
+    
+            if ($res) {
+                Helper::redirect('/')
+            }
+        }
+        $this->render('home');
+    }
+}
--- core/Helper.php
+++ core/Helper.php
@@ -10,4 +10,33 @@ class Helper {
         header("Location: $url");
         exit();
     }
+
+    public static function uploadFile($file, $destinationPath) {
+        
+        //Config image 
+        $maxImageSize = 5 * 1024 * 1024; //5Mo
+        $allowedImageTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
+
+        $imageName      = $image['name']; //yoyo.png
+        $imageTmpPath   = $image['tmp_name']; //chemin tampon
+        $imageSize      = $image['size']; //Taille de l'image
+        $imageType      = mime_content_type($imageTmpPath); //Type de l'image
+
+        if($imageSize > $maxImageSize){
+            return ["error" => "L'image est trop lourde"];
+        }
+        if(!in_array($imageType, $allowedImageTypes)){
+            return ["error" => "Le type de fichier n'est pas autorisé."];
+        }
+
+        $imageName = time() . '_' . basename($imageName);
+
+        $destinationPath = BASE_URL . '/' . $destinationPath;
+
+        if (!move_uploaded_file($imageTmpPath, $destinationPath)) {
+            return ["error" => "Erreur lors de l'envoi de l'image."];
+        }
+
+        return $imageName;
+    }
 }
