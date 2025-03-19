251f6dc747032eafd7da15a8a0f3d182fc0e5e2e
eliewebavenue <elie@webavenue.agency>
2025-03-19 16:04:30 +0100

    add post form on home page view

--- views/homeView.php
+++ views/homeView.php
@@ -1,8 +1,29 @@
 <?php include __DIR__ . '/partials/header.php' ?>
 <div class="container col-lg-4 my-4">
+
+    <!-- Formulaire d'ajout de post -->
+    <div class="card my-4">
+        <div class="card-body">
+            <form method="post" action="/post/add" enctype="multipart/form-data" >
+                <div class="mb-4">
+                    <label for="postImage" class="form-label">Choisir une image</label>
+                    <input class="form-control form-control-lg" id="postImage" type="file">
+                </div>
+
+                <div class="mb-4">
+                    <label class="form-label" for="legende">Description</label>
+                    <textarea id="legende" name="legende" class="form-control" required ></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Poster</button>
+
+            </form>
+        </div>
+    </div>
+
     <?php if(!empty($posts)) : ?>
         <?php foreach ($posts as $post) : ?>
-            <div class="card mb-3" style="width: 18rem;">
+            <div class="card mb-3">
                 <img src="<?php echo BASE_URL . "/post-img/" . $post['image'] ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <p class="card-text"><?php echo $post['legende'] ?></p>
@@ -10,6 +31,5 @@
             </div> 
         <?php endforeach; ?>
     <?php endif; ?>
-    
 </div>
 <?php include __DIR__ . '/partials/footer.php' ?>
